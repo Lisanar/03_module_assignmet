3e4ef3a8cce8be62a62ab826733de12adbdc849d
Yasqi Ghifari <yasqighifari@gmail.com>
2021-01-25 18:09:18 +0700

    first commit 03_assignment

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import Validation from './Validation/Validation'
+
+class App extends Component {
+  state = {
+    userInput: ''
+  }
+  
+  inputChangedHandler = (event) => {
+    this.setState({userInput: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="text"
+        onChange={this.inputChangedHandler} 
+        value={this.state.userInput}></input>
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length}/>
+      </div>
+    );
+  }
+}
+
+export default App;
